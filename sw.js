e21e4eb374f8279f17491a9b8067e8a23c97b5a7
Mata Elang <matalang@local>
2026-07-07 11:53:44 +0700

    Desktop: wider layout + bigger photos (v52)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Network-first service worker (biar update file kebaca pas online) */
-const CACHE = 'ac-service-v51';
+const CACHE = 'ac-service-v52';
 const ASSETS = [
   './',
   './index.html',
